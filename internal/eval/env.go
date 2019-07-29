510ca839043282529ae2355a7a3e5c11c98d2bab
Mathias Hörtnagl <mathias.hoertnagl@gmail.com>
2019-07-29 23:29:04 +0200

    eval def and let

--- internal/eval/env.go
+++ internal/eval/env.go
@@ -1,14 +1,35 @@
 package eval
 
+import (
+	"github.com/mhoertnagl/splis2/internal/read"
+)
+
 type Env interface {
-	Set()
-	Lookup()
+	Set(name string, val read.Node)
+	Lookup(name string) read.Node
 }
 
 type env struct {
 	outer Env
+  defs map[string]read.Node
 }
 
-func NewEnv() Env {
-	return &env{}
+func NewEnv(outer Env) Env {
+	return &env{outer: outer, defs: make(map[string]read.Node)}
+}
+
+func (e *env) Set(name string, val read.Node) {
+  if _, ok := e.defs[name]; !ok {
+    e.defs[name] = val
+  }
+}
+
+func (e *env) Lookup(name string) read.Node {
+  if v, ok := e.defs[name]; ok {
+    return v
+  } else if e.outer != nil {
+    return e.outer.Lookup(name)
+  }
+  // TODO: Return error node?
+  return nil
 }
--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -1,6 +1,7 @@
 package eval
 
 import (
+  "fmt"
 	"github.com/mhoertnagl/splis2/internal/read"
 )
 
@@ -9,10 +10,17 @@ type Evaluator interface {
 }
 
 type evaluator struct {
+  err []*read.ErrorNode
 }
 
 func NewEvaluator() Evaluator {
 	return &evaluator{}
+}
+
+func (e *evaluator) error(format string, args ...interface{}) read.Node {
+	err := &read.ErrorNode{Msg: fmt.Sprintf(format, args...)}
+	e.err = append(e.err, err)
+	return err
 }
 
 func (e *evaluator) Eval(env Env, node read.Node) read.Node {
@@ -27,13 +35,17 @@ func (e *evaluator) eval(env Env, node read.Node) read.Node {
 		return e.evalVector(env, n)
 	case *read.HashMapNode:
 		return e.evalHashMap(env, n)
-	default:
+  case *read.SymbolNode:
+	   return e.evalSymbol(env, n)
+  default:
 		// Return unchanged. These are immutable atoms.
 		return n
 	}
 }
 
 func (e *evaluator) evalList(env Env, n *read.ListNode) read.Node {
+  // TODO: switch ...
+  
 	return &read.ListNode{Items: e.evalSeq(env, n.Items)}
 }
 
@@ -43,6 +55,7 @@ func (e *evaluator) evalVector(env Env, n *read.VectorNode) read.Node {
 
 func (e *evaluator) evalHashMap(env Env, n *read.HashMapNode) read.Node {
 	c := &read.HashMapNode{Items: make(map[read.Node]read.Node)}
+  // TODO: to separate func?
 	for key, val := range n.Items {
 		k := e.eval(env, key)
 		v := e.eval(env, val)
@@ -57,4 +70,65 @@ func (e *evaluator) evalSeq(env Env, items []read.Node) []read.Node {
 		res = append(res, e.eval(env, item))
 	}
 	return res
+}
+
+func (e *evaluator) evalSymbol(env Env, n *read.SymbolNode) read.Node {
+  if v := env.Lookup(n.Name); v != nil {
+    return v
+  }
+  return e.error("Undefined variable [%s].", n.Name)
+}
+
+// evalDef binds a name to a value. Redefinitions of the same name in the same
+// environment will be ignored silently.
+// (def! a 42) will bind a to 42 in the current environment. Returns the bound
+// value 42.
+func (e *evaluator) evalDef(env Env, n *read.ListNode) read.Node {
+  return e.evalSet(env, n.Items[1], n.Items[2])
+}
+
+// evalSet evaluates the name and the val argument and binds name to val in the 
+// environment.
+func (e *evaluator) evalSet(env Env, name read.Node, val read.Node) read.Node {
+  n := e.eval(env, name)
+  v := e.eval(env, val)
+  switch x := n.(type) {
+  case *read.SymbolNode:
+    env.Set(x.Name, v)
+    return v
+  default:
+    return e.error("Cannot bind to [%s].", name)
+  }
+}
+
+// evalLet binds a list, vector or hash-map of pairs to a noe local environment
+// and evaluates it's body in it.
+// If the second argument is neiher a list, vector or hash-map this it yields a 
+// runtime error.
+func (e *evaluator) evalLet(env Env, n *read.ListNode) read.Node {
+  sub := NewEnv(env)
+  bindings := e.eval(env, n.Items[1])
+  switch b := bindings.(type) {
+  case *read.ListNode:
+    e.evalSeqBindings(sub, b.Items)
+  case *read.VectorNode:
+    e.evalSeqBindings(sub, b.Items)
+  case *read.HashMapNode:
+    e.evalHashMapBindings(sub, b.Items)
+  default:
+    return e.error("Cannot bind non-sequence.")
+  }
+  return e.eval(sub, n.Items[2])
+}
+
+func (e *evaluator) evalSeqBindings(env Env, b []read.Node) {
+  for i := 0; i < len(b); i+=2 {
+    e.evalSet(env, b[i], b[i+1]) 
+  }  
+}
+
+func (e *evaluator) evalHashMapBindings(env Env, b map[read.Node]read.Node) {
+  for k, v := range b {
+    e.evalSet(env, k, v)  
+  }  
 }
